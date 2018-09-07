3c58176e06993ee74e1186f2a758b0495f2c9fb4
Julio Berina <julioberina@gmail.com>
2018-09-06 20:10:15 -0700

    Get bidi to work with http-kit

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [bidi "2.1.4"]
+                 [http-kit "2.2.0"]
+                 [ring/ring-core "1.6.3"]]
+  
   :main ^:skip-aot questionnaire.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/questionnaire/core.clj
+++ src/questionnaire/core.clj
@@ -1,7 +1,22 @@
 (ns questionnaire.core
+  (:require [bidi.ring :as br]
+            [bidi.bidi :as bd]
+            [org.httpkit.server :refer :all]
+            [ring.util.response :refer :all])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn display-response [body]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body body})
+
+(defn handler [req]
+  (let [routes ["/" {"" :index
+                     "about" :about}]]
+    (case (:handler (bd/match-route routes (:uri req)))
+      :index (display-response "<h1>Index</h1>")
+      :about (display-response "<p>About</p>"))))
+
+(defn -main [& args]
+  (let [server (run-server handler {:port 8000})]
+    (println "Server started at port 8000")))
